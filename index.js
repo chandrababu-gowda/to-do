1c5bb9778d310bc4b989b7ac3e83357a1c459049
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2022-10-23 15:14:35 +0530

    Add the value to the box

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const text = document.getElementById("inputWork");
 const btn = document.getElementById('submitBtn');
+const list = document.getElementById('list1');
 
 btn.addEventListener('click',()=>{
     let textValue = text.value;
     console.log(textValue);
+    list.insertAdjacentHTML('afterend',`<p>${textValue}</p>`)
 })
 
 
