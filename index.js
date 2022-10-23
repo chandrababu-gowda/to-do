36afd25c9c25737f44db1a34e469465a3d45de30
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2022-10-23 14:59:19 +0530

    Capture the value

---
+++ index.js
@@ -0,0 +1,17 @@
+const text = document.getElementById("inputWork");
+const btn = document.getElementById('submitBtn');
+
+btn.addEventListener('click',()=>{
+    let textValue = text.value;
+    console.log(textValue);
+})
+
+
+
+
+
+
+
+
+
+
